3e7cd119e2447e61ab670a27dd15dbda3ce7a4e9
jeetpopat222-art <jeetpopat222@gmail.com>
2026-03-10 15:31:33 +0530

    made almost working prototype(:

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
 import sys
-print("Welcome to the Quiz Game !\n" )
+name = input("enter your name: ")
+print(f"Welcome to the Quiz Game {name}!\n" )
+
+
+right_answer = 0
+wrong_answer = 0
 
 print("1.enter the game")
 print("2.exit")
@@ -8,23 +13,24 @@ user = int(input("enter your command: "))
 if user == 2:
        sys.exit() 
 else:
-    name = input("enter your name: ")
+      with open("question.txt") as f:
+        ques = f.readlines()
+        for i in range(0,40,2):
+                        question = ques[i].strip().lower()
+                        print(question)
+                        ask_ans = input("enter your answer: ").lower()
+                        if ask_ans == "ttt":
+                                break
+                        for i in range(1,41,2):
+                                answer = ques[i].strip().lower()
+                                if ask_ans == answer:
+                                        right_answer += 1
+                                else:
+                                        wrong_answer +=1
 
 
-      
 
-with open("question.txt") as f:
-                line = f.readlines()
-                print(line[0])
-                ask_ans = input("enter your answer:").lower()
-                with open("question.txt") as f:
-                        answer = f.readlines()
-                        real_ans = answer[1]
-                        if ask_ans == real_ans:
-                                print("correct answer")
                         
-                        
-
 
         
         
